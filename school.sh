bb2c361abfccf81e1e249bec6874cf7c2e057e83
Yubo Zhang <yzhang2992@wisc.edu>
2026-03-05 17:51:47 -0600

    write school.sh to compute MADISON SCHOOLS average

--- school.sh
+++ school.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d, -f7 | {
+    sum=0
+    count=0
+    while read x
+    do
+        sum=$((sum + x))
+        count=$((count + 1))
+    done
+    echo $((sum / count))
+}
