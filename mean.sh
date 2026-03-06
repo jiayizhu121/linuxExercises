3d9004df0a05fe3cbc7ed3bda7f4a5ebbf334a5e
Yubo Zhang <yzhang2992@wisc.edu>
2026-03-05 18:00:49 -0600

    write mean.sh to compute column mean from csv

--- mean.sh
+++ mean.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]
+then
+    echo "usage: ./mean.sh <column> [file.csv]" >&2
+    exit 1
+fi
+
+column=$1
+
+if [ $# -eq 2 ]
+then
+    file=$2
+else
+    file=/dev/stdin
+fi
+
+cut -d, -f"$column" "$file" | tail -n +2 | {
+    sum=0
+    count=0
+    while read x
+    do
+        sum=$((sum + x))
+        count=$((count + 1))
+    done
+    echo $((sum / count))
+}
